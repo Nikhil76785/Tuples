f2146b3f3a0389bb5d0b18cd0d71e8dc71f81fad
Nikhil76785 <valivetinikhil2@gmail.com>
2025-08-11 19:49:27 +0530

    TUPLE FUNCTIONS

---
+++ tuple1.py
@@ -0,0 +1,15 @@
+tuple = ('Hello', 785, 76.785, False)
+print(tuple)
+
+tuple2 = (8, 5, 2, 5, 1, 7, 9, 0, 34, 2)
+print(tuple2)
+
+tuple2 = tuple2 + (55, )
+print(tuple2)
+
+tuple3 = (34, 73, 46, 12, 73, 5, 88, 34, 73)
+print(tuple3.count(73))
+
+tuple4 = (1, 3, 5, 6, 2, 4, 9, 0)
+_slicing = tuple4[3:5]
+print(_slicing)
